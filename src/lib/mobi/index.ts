116cf33b81b50f513a19a462dcb8d2769e845a21
sunnylost <sunnylost@gmail.com>
2023-03-26 00:06:16 +0800

    fix: improve mobi parse

--- src/lib/mobi/index.ts
+++ src/lib/mobi/index.ts
@@ -1,12 +1,32 @@
-import type { Book } from '../types'
+/**
+ * https://wiki.mobileread.com/wiki/PDB
+ * https://wiki.mobileread.com/wiki/MOBI
+ */
 
+import type { Book } from '@/types'
+import { PDBHeader, PalmHeader, MobiHeader } from './types'
+
+class MobiFile {
+    pdbHeader: PDBHeader
+    palmHeader: PalmHeader
+    header: MobiHeader
+    records: {
+        offset: number
+        attr: number
+    }[] = []
+
+    constructor() {
+        this.pdbHeader = {} as PDBHeader
+        this.palmHeader = {} as PalmHeader
+        this.header = {} as MobiHeader
+    }
+}
+
+let mobi: MobiFile
 const decoder = new TextDecoder('utf-8')
 let offset = 0
 let dataView: DataView
-let records: {
-    offset: number
-    attr: number
-}[] = []
+let fileTitle: string
 
 function jumpTo(_offset: number) {
     offset = _offset
@@ -14,6 +34,10 @@ function jumpTo(_offset: number) {
 
 function readString(length: number) {
     return decoder.decode(dataView.buffer.slice(offset, (offset += length)))
+}
+
+function peakString(length: number) {
+    return decoder.decode(dataView.buffer.slice(offset, offset + length))
 }
 
 function readBytes(num: number) {
@@ -28,113 +52,127 @@ function readBytes(num: number) {
         result = dataView.getBigUint64(offset)
     }
 
-    offset += num
-    return result
+    skip(num)
+    return result as number
 }
 
 function skip(num: number) {
     offset += num
 }
 
-function parseMobiFile() {
-    console.log('name:', readString(32))
-    console.log('attributes:', readBytes(2))
-    console.log('version:', readBytes(2))
-    console.log('creation date:', readBytes(4))
-    console.log('modification date:', readBytes(4))
-    console.log('last backup date:', readBytes(4))
-    console.log('modification Number:', readBytes(4))
-    console.log('appinfo ID:', readBytes(4))
-    console.log('sortInfo ID:', readBytes(4))
-    console.log('type:', readString(4))
-    console.log('creator:', readString(4))
-    console.log('unique IDseed:', readBytes(4))
-    console.log('next record list ID:', readBytes(4))
-    const numberOfRecords = readBytes(2) as number
-    console.log('number of records:', numberOfRecords)
+function parsePDBHeader() {
+    const { pdbHeader } = mobi
+    pdbHeader.name = readString(32)
+    pdbHeader.attributes = readBytes(2)
+    pdbHeader.version = readBytes(2)
+    pdbHeader.createTime = readBytes(4)
+    pdbHeader.modificationTime = readBytes(4)
+    pdbHeader.lastBackupTime = readBytes(4)
+    pdbHeader.modificationNumber = readBytes(4)
+    pdbHeader.appInfoId = readBytes(4)
+    pdbHeader.sortInfoId = readBytes(4)
+    pdbHeader.type = readString(4)
+    pdbHeader.creator = readString(4)
+    pdbHeader.uniqueIDSeed = readBytes(4)
+    pdbHeader.nextRecordListId = readBytes(4)
+    pdbHeader.numberOfRecords = readBytes(2)
+}
 
-    console.log('---------RECORD--------')
+function parseMobiRecords() {
     let currentRecordIndex = 0
-    while (currentRecordIndex <= numberOfRecords) {
-        // console.log(`RECORD ${currentRecordIndex}`)
-        records.push({
-            offset: readBytes(4) as number,
-            attr: readBytes(4) as number,
+    while (currentRecordIndex < mobi.pdbHeader.numberOfRecords) {
+        mobi.records.push({
+            offset: readBytes(4),
+            attr: readBytes(4),
         })
-        // console.log('record data offset:', readBytes(4))
-        // console.log('record attributes:', readBytes(4)) // TODO: 1 attr, 3 unique id
-        // console.log('unique ID:', readBytes(3))
         currentRecordIndex++
     }
-    console.log(records)
-    jumpTo(records[0].offset)
+}
 
-    // palm doc header
-    console.log('PalmDoc Header')
-    console.log('compression:', readBytes(2))
-    console.log('unused:', readBytes(2))
-    console.log('text length:', readBytes(4))
-    console.log('record count:', readBytes(2))
-    console.log('record size:', readBytes(2))
-    console.log('current position:', readBytes(4))
-    // console.log('encryption', readBytes(2))
-    // console.log('unknown', readBytes(2))
+function parsePalDocHeader() {
+    const { palmHeader } = mobi
+    palmHeader.compression = readBytes(2)
+    palmHeader.unused = readBytes(2)
+    palmHeader.textLength = readBytes(4)
+    palmHeader.recordCount = readBytes(2)
+    palmHeader.recordSize = readBytes(2)
+    palmHeader.currentPosition = readBytes(4)
+}
 
-    const startOffset = offset
-    console.log('MOBI Header')
-    console.log('Identifier:', readString(4))
-    console.log('Header length:', readBytes(4))
-    console.log('mobi type:', readBytes(4))
-    console.log('text encoding:', readBytes(4))
-    console.log('unique id:', readBytes(4))
-    console.log('file version:', readBytes(4))
-    console.log('ortographic index:', readBytes(4))
-    console.log('inflection index:', readBytes(4))
-    console.log('index names:', readBytes(4))
-    console.log('index keys:', readBytes(4))
-    console.log('extra index0:', readBytes(4))
-    console.log('extra index1:', readBytes(4))
-    console.log('extra index2:', readBytes(4))
-    console.log('extra index3:', readBytes(4))
-    console.log('extra index4:', readBytes(4))
-    console.log('extra index5:', readBytes(4))
-    console.log('first none-book index:', readBytes(4))
-    console.log('full name offset:', readBytes(4))
-    console.log('full name length:', readBytes(4))
-    console.log('locale:', readBytes(4))
-    console.log('input language:', readBytes(4))
-    console.log('output language:', readBytes(4))
-    console.log('mini version:', readBytes(4))
-    console.log('first image index:', readBytes(4))
-    console.log('huffman record offset:', readBytes(4))
-    console.log('huffman record count:', readBytes(4))
-    console.log('huffman table offset:', readBytes(4))
-    console.log('huffman table length:', readBytes(4))
+function parseMobiHeader() {
+    const { header } = mobi
+    const _offset = offset
+    header.identifier = readString(4)
+    header.headerLength = readBytes(4)
+    header.mobiType = readBytes(4)
+    header.textEncoding = readBytes(4)
+    header.uniqueID = readBytes(4)
+    header.fileVersion = readBytes(4)
+    header.ortographicIndex = readBytes(4)
+    header.inflectionIndex = readBytes(4)
+    header.indexNames = readBytes(4)
+    header.indexKeys = readBytes(4)
+    header.extraIndex0 = readBytes(4)
+    header.extraIndex1 = readBytes(4)
+    header.extraIndex2 = readBytes(4)
+    header.extraIndex3 = readBytes(4)
+    header.extraIndex4 = readBytes(4)
+    header.extraIndex5 = readBytes(4)
+    header.firstNoneBookIndex = readBytes(4)
+    header.fullNameOffset = readBytes(4)
+    header.fullNameLength = readBytes(4)
+    header.locale = readBytes(4)
+    header.inputLanguage = readBytes(4)
+    header.outputLanguage = readBytes(4)
+    header.miniVersion = readBytes(4)
+    header.firstImageIndex = readBytes(4)
+    header.huffmanRecordOffset = readBytes(4)
+    header.huffmanRecordCount = readBytes(4)
+    header.huffmanTableOffset = readBytes(4)
+    header.huffmanTableLength = readBytes(4)
+    header.exthFlags = readBytes(4)
 
-    const hasExthHeader = readBytes(4) === 6
-    console.log('EXTH flags:', hasExthHeader)
-
-    console.log('unknown:')
     skip(32)
-    console.log('unknown:', readBytes(4))
-    console.log('DRM offset:', readBytes(4))
-    console.log('DRM count:', readBytes(4))
-    console.log('DRM size:', readBytes(4))
-    console.log('DRM flags:', readBytes(4))
-    skip(8)
-    console.log('first content record number:', readBytes(2))
-    console.log('last content record number:', readBytes(2))
     skip(4)
-    console.log('FLIS record number:', readBytes(4))
-    console.log('unknown FLIS record count?:', readBytes(4))
+    header.drmOffset = readBytes(4)
+    header.drmCount = readBytes(4)
+    header.drmSize = readBytes(4)
+    header.drmFlags = readBytes(4)
+    skip(8)
+    header.firstContentRecordNumber = readBytes(2)
+    header.lastContentRecordNumber = readBytes(2)
+    skip(4)
+    header.flisRecordNumber = readBytes(4)
+    header.unknownFlisRecordCount = readBytes(4)
     skip(8)
     skip(4)
-    console.log('first compilation data section count:', readBytes(4))
-    console.log('number of compilation data sections:', readBytes(4))
-    console.log('unknown:', readBytes(4))
-    console.log('extra record data flags:', readBytes(4))
-    console.log('INDX record offset:', readBytes(4))
-    skip(24)
+    header.firstCompilationDataSectionCount = readBytes(4)
+    header.numberOfCompilationDataSections = readBytes(4)
+    skip(4)
+    header.extraRecordDataFlags = readBytes(4)
+    header.indxRecordOffset = readBytes(4)
+    jumpTo(_offset + header.headerLength)
+}
+
+// skip
+function parseExthHeader() {
+    if (peakString(4) !== 'EXTH') {
+        return
+    }
+
+    const _offset = offset
+    console.log(readString(4))
+    const headerLength = readBytes(4)
+    console.log('exth header length', headerLength)
+    let count = readBytes(4)
+    console.log('record count', count)
+
+    while (count--) {
+        console.log('record type', readBytes(4))
+        const recordLength = readBytes(4)
+        console.log('record length', recordLength)
+        console.log('record data', readString(recordLength - 8))
+    }
 
     if (hasExthHeader) {
     }
@@ -142,7 +180,7 @@ function parseMobiFile() {
 export function handleMobi(file: File): Book {
     const reader = new FileReader()
     reader.onload = (e) => {
-        const data = e.target.result
+        const data = e.target?.result
 
         if (!data) {
             console.log('empty')
@@ -153,5 +191,12 @@ export function handleMobi(file: File): Book {
         parseMobiFile()
     }
     reader.readAsArrayBuffer(file)
+
+    return {
+        // title: fileTitle,
+    }
 }
-export async function parse(rawContent: ArrayBuffer): Promise<Book> {}
+export async function parse(rawContent: File): Promise<Book> {
+    mobi = new MobiFile()
+    return handleMobi(rawContent)
+}
---
+++ src/lib/mobi/types.ts
@@ -0,0 +1,69 @@
+export type PDBHeader = {
+    name: string
+    attributes: number
+    version: number
+    createTime: number
+    modificationTime: number
+    lastBackupTime: number
+    modificationNumber: number
+    appInfoId: number
+    sortInfoId: number
+    type: string
+    creator: string
+    uniqueIDSeed: number
+    nextRecordListId: number
+    numberOfRecords: number
+}
+
+export type PalmHeader = {
+    compression: number
+    unused: number
+    textLength: number
+    recordCount: number
+    recordSize: number
+    currentPosition: number
+}
+
+export type MobiHeader = {
+    identifier: string
+    headerLength: number
+    mobiType: number
+    textEncoding: number
+    uniqueID: number
+    fileVersion: number
+    ortographicIndex: number
+    inflectionIndex: number
+    indexNames: number
+    indexKeys: number
+    extraIndex0: number
+    extraIndex1: number
+    extraIndex2: number
+    extraIndex3: number
+    extraIndex4: number
+    extraIndex5: number
+    firstNoneBookIndex: number
+    fullNameOffset: number
+    fullNameLength: number
+    locale: number
+    inputLanguage: number
+    outputLanguage: number
+    miniVersion: number
+    firstImageIndex: number
+    huffmanRecordOffset: number
+    huffmanRecordCount: number
+    huffmanTableOffset: number
+    huffmanTableLength: number
+    exthFlags: number
+    drmOffset: number
+    drmCount: number
+    drmSize: number
+    drmFlags: number
+    firstContentRecordNumber: number
+    lastContentRecordNumber: number
+    flisRecordNumber: number
+    unknownFlisRecordCount: number
+    firstCompilationDataSectionCount: number
+    numberOfCompilationDataSections: number
+    extraRecordDataFlags: number
+    indxRecordOffset: number
+}
