195560b186129e025d4b75d3c05c48489fd37c58
sunnylost <sunnylost@gmail.com>
2023-03-30 20:57:50 +0800

    chore: upgrade dependencies, update tailwindcss config file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+export default {
     content: ['./src/**/*.{ts,tsx}'],
     theme: {
         extend: {
@@ -14,4 +15,4 @@ module.exports = {
         },
     },
     plugins: [],
-}
+} satisfies Config
