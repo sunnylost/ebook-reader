b1df9b645c0413d6e03b227a8e88693ffffcdadc
sunnylost <sunnylost@gmail.com>
2023-04-05 23:52:03 +0800

    chore: remove unused codes

--- src/options/index.tsx
+++
@@ -1,7 +0,0 @@
-/* @refresh reload */
-import { render } from 'solid-js/web'
-
-import './index.css'
-import { Options } from './Options'
-
-render(() => <Options />, document.getElementById('app') ?? document.body)
