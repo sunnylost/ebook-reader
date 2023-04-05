3e453798a14d39238a915ae87f969f45a91ff8b0
sunnylost <sunnylost@gmail.com>
2023-04-05 23:52:23 +0800

    feat: support paging

--- src/components/book.tsx
+++ src/components/book.tsx
@@ -1,36 +1,68 @@
-import { For, Match, Switch } from 'solid-js'
+import { Match, Show, Switch } from 'solid-js'
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'solid-icons/ai'
+import { bookState, nextPage, prevPage } from '@/stores/book'
 import { FloatToolBar } from './float-toolbar/index'
 import { Toc } from './toc'
 import { Book as BookType } from '@/types'
+import { Button } from '@/components/button'
 
 // TODO
 export function Book(props: { book: BookType | null }) {
     return (
         <div class="book relative w-[1000px] m-auto px-10 bg-[#1e1818] text-white">
-            <div class="book-content">
-                <For each={props.book?.entries}>
-                    {(item) => (
-                        <div class="entry-wrap">
-                            <Switch>
-                                <Match when={item.type === 'css'}>
-                                    <link
-                                        rel="stylesheet"
-                                        href={item.content}
-                                    ></link>
-                                </Match>
-                                <Match when={item.type === 'html'}>
-                                    <div
-                                        class="text-left"
-                                        innerHTML={item.content}
-                                    ></div>
-                                </Match>
-                            </Switch>
-                        </div>
-                    )}
-                </For>
-            </div>
-            <Toc content={props.book?.toc}></Toc>
-            <FloatToolBar></FloatToolBar>
+            <Show when={bookState.currentPageContent}>
+                <div class="book-content">
+                    <div class="entry-wrap">
+                        <Switch>
+                            <Match
+                                when={
+                                    bookState.currentPageContent?.type === 'css'
+                                }
+                            >
+                                <link
+                                    rel="stylesheet"
+                                    href={bookState.currentPageContent?.content}
+                                ></link>
+                            </Match>
+                            <Match
+                                when={
+                                    bookState.currentPageContent?.type ===
+                                    'html'
+                                }
+                            >
+                                <div
+                                    class="text-left"
+                                    innerHTML={
+                                        bookState.currentPageContent?.content ||
+                                        ''
+                                    }
+                                ></div>
+                            </Match>
+                        </Switch>
+                    </div>
+                    <div class="flex justify-center items-center gap-16 py-16">
+                        <Show when={bookState.hasPrevPage}>
+                            <Button
+                                onClick={() => prevPage()}
+                                tooltip="Prev Page"
+                            >
+                                <AiOutlineArrowLeft />
+                            </Button>
+                        </Show>
+
+                        <Show when={bookState.hasNextPage}>
+                            <Button
+                                onClick={() => nextPage()}
+                                tooltip="Next Page"
+                            >
+                                <AiOutlineArrowRight />
+                            </Button>
+                        </Show>
+                    </div>
+                </div>
+                <Toc content={props.book?.toc}></Toc>
+                <FloatToolBar></FloatToolBar>
+            </Show>
         </div>
     )
 }
--- src/stores/book.ts
+++ src/stores/book.ts
@@ -1,6 +1,6 @@
 import { createStore } from 'solid-js/store'
 import { parseBook } from '@/lib'
-import type { Book } from '@/types'
+import type { Book, BookContentEntry } from '@/types'
 
 export const BOOK_STATUS = {
     init: 'init',
@@ -13,12 +13,36 @@ type BookStore = {
     status: keyof typeof BOOK_STATUS
     currentOpenedBook: Book | null
     error: string
+    totalPageNum: number
+    currentPageNum: number
+    hasNextPage: boolean
+    hasPrevPage: boolean
+    currentPageContent: BookContentEntry | null
 }
 
 export const [bookState, updateBookState] = createStore<BookStore>({
     status: BOOK_STATUS.init,
     currentOpenedBook: null,
     error: '',
+    totalPageNum: 0,
+    currentPageNum: 0,
+
+    get hasNextPage() {
+        return this.currentPageNum < this.totalPageNum
+    },
+
+    get hasPrevPage() {
+        return this.currentPageNum > 0
+    },
+
+    get currentPageContent() {
+        console.log(
+            this.currentOpenedBook?.entries,
+            this.currentPageNum,
+            this.totalPageNum
+        )
+        return this.currentOpenedBook?.entries[this.currentPageNum]
+    },
 })
 
 export async function openBook(event: Event) {
@@ -31,9 +55,12 @@ export async function openBook(event: Event) {
         })
         // const bookId = await generateFileUrl(uploadFile)
 
+        const book = await parseBook(uploadFile.name, uploadFile)
         updateBookState({
             status: BOOK_STATUS.done,
-            currentOpenedBook: await parseBook(uploadFile.name, uploadFile),
+            currentPageNum: 0,
+            totalPageNum: book.entries.length,
+            currentOpenedBook: book,
         })
     } catch (e) {
         console.log(e)
@@ -53,4 +80,20 @@ export function resetBookState() {
         status: BOOK_STATUS.init,
         error: '',
     })
+}
+
+export function nextPage() {
+    if (bookState.hasNextPage) {
+        updateBookState({
+            currentPageNum: bookState.currentPageNum + 1,
+        })
+    }
+}
+
+export function prevPage() {
+    if (bookState.hasPrevPage) {
+        updateBookState({
+            currentPageNum: bookState.currentPageNum - 1,
+        })
+    }
 }
