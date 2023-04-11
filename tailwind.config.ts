a420f01675bc5199577a36e79c51f8740226d699
sunnylost <sunnylost@gmail.com>
2023-04-12 00:43:46 +0800

    fix: update theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,15 +1,30 @@
 import type { Config } from 'tailwindcss'
 
 export default {
+    darkMode: 'class',
     content: ['./src/**/*.{ts,tsx}'],
     theme: {
         extend: {
             colors: {
-                bg: '#2D2727',
-                primary: '#1e1818',
+                bg: '#e2e8f0',
+                'dark-bg': '#020617',
+                primary: '#f1f5f9',
+                'dark-primary': '#0f172a',
+                text: '#030712',
+                'dark-text': '#f3f4f6',
             },
             spacing: {
+                2: '2px',
+                4: '4px',
+                6: '6px',
+                8: '8px',
+                10: '10px',
                 12: '12px',
+                14: '14px',
+                16: '16px',
+                18: '18px',
+                20: '20px',
+                24: '24px',
                 36: '36px',
             },
         },
